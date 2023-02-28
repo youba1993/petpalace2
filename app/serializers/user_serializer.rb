9f4f8176814c3b7d5cc8a76d8f6643cdf1af7e87
youba idjis <youba.etud@gmail.com>
2023-02-28 00:08:12 -0800

    configuring devise-jwt

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,7 @@
 class UserSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :id, :email, :created_at
+  attributes :id, :email
+  attribute :created_date do |user|
+    user && user.created_at.strftime('%d/%m/%Y')
+end
 end
