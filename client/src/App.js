d8b3b9796e67a9709c4f05f491001062af8f7cd2
youba idjis <youba.etud@gmail.com>
2023-02-27 22:40:17 -0800

    adding TailwindCss

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+     <div className="flex flex-col items-center mt-16">
+       <h1 className="text-4xl text-center">
+         Pet Palace
+       </h1>
+       <button className="bg-black text-white p-2.5 w-fit mt-9 ">
+         Get Started
+       </button>
+     </div>
     </div>
   );
 }
