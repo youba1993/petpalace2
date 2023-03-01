85a6cdc2b7b1e5bc7ad57ac91bd4d949fe73a89d
youba idjis <youba.etud@gmail.com>
2023-03-01 10:24:01 -0800

    Adding ProductListPage

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import HomePage from './pages/HomePage';
-// import ProductListPage from './pages/ProductListPage';
+import ProductListPage from './pages/ProductListPage';
 // import ProductDetailPage from './pages/ProductDetailPage';
 // import CartPage from './pages/CartPage';
 // import CheckoutPage from './pages/CheckoutPage';
@@ -19,8 +19,8 @@ function App() {
         <main className="container flex-1 mx-auto px-4">
           <Routes>
             <Route exact path="/" element={<HomePage />}/>
-            {/* <Route exact path="/products" element={<ProductListPage />}/>
-            <Route path="/products/:id" element={<ProductDetailPage />}/>
+            <Route exact path="/products" element={<ProductListPage />}/>
+            {/* <Route path="/products/:id" element={<ProductDetailPage />}/>
             <Route path="/cart" element={<CartPage />}/>           
             <Route path="/checkout" element={<CheckoutPage />}/>
             <Route path='/login' element={<Login />} />
---
+++ client/src/pages/ProductListPage.js
@@ -0,0 +1,61 @@
+import React, { useState, useEffect } from 'react';
+import Skeleton from 'react-loading-skeleton';
+
+function ProductList() {
+    const [products, setProducts] = useState([]);
+    const [loading, setLoading] = useState(true);
+    
+
+    useEffect(() => {
+        fetch('/products')
+            .then(response => response.json())
+            .then(data => {
+                setTimeout(() => {
+                    setProducts(data);
+                    setLoading(false);
+                  }, 1000);
+            });
+    }, []);
+
+    const handleAddToCart = () =>{
+        console.log("added to cart")
+    }
+
+    return (
+        <div className="p-8 container mx-auto">
+        <h1 className="text-2xl font-bold mb-4 ">Find everything your Pet needs at PetPalace</h1>
+            {loading ? (
+                <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4">
+                    {Array.from({ length: 8 }).map((_, index) => (
+                        <div key={index} className="bg-white shadow-md rounded-md p-4">
+                            <Skeleton height={120} />
+                            <Skeleton height={16} width="80%" className="mt-2" />
+                            <Skeleton height={16} width="60%" className="mt-1" />
+                        </div>
+                    ))}
+                </div>
+            ) : (
+                <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4">
+                    {products.map((product) => (
+                        <div key={product.id} className="bg-white shadow-md rounded-md p-4">
+                            <img src={product.image_url} alt={product.name} className="w-full h-48 object-cover rounded-md" />
+                            <h2 className="text-gray-800 font-medium text-lg mt-2">{product.name}</h2>
+                            <p className="text-gray-500 text-sm mt-1">{product.description}</p>
+                            <div className="flex justify-between items-center mt-4">
+                                <span className="text-gray-700 font-semibold">${product.price}</span>
+                                <button
+                                    className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded"
+                                    onClick={() => handleAddToCart(product)}
+                                >
+                                    Add to Cart
+                                </button>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            )}
+        </div>
+    );
+}
+
+export default ProductList;
